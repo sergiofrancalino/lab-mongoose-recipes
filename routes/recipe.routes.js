c981dc3d310dec9ee7492f85fa0300c71f3a6722
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-11-30 19:39:22 -0300

    Refeito .env e a Interation 2 ok

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -2,5 +2,23 @@ import express from "express";
 import RecipeModel from "../models/Recipe.model.js";
 
 const recipeRoute = express.Router();
+//HERE THE ROUTES
+
+// Iteration 2 - Create a recipe
+recipeRoute.post('/create-recipe', async (req, res) => {
+  try {
+    const form = req.body;
+
+    //quer criar um documento dentro da sua collection -> .create()
+    const newRecipe = await RecipeModel.create(form);
+
+    return res.status(201).json(newRecipe);
+  } catch (error) {
+    console.log(error.errors);
+    return res.status(500).json(error.errors);
+  }
+});
+
+//Iteration 3 - Insert multiple recipes
 
 export default recipeRoute;
